a390d7e755d110b897e0bce41d539279ad5245fe
Tarun Vellishetty <tvellishetty@gitlab.com>
2024-05-04 16:11:01 +0530

    add docs page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Greet } from "../wailsjs/go/main/App";
+
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇"
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
 
-    return (
-        <div id="App">
-            <div id="result" className="result underline">{resultText}</div>
-            <div id="input" className="input-box">
-                <Input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text" />
-                <button className="btn" onClick={greet}>Greet</button>
+  return (
+    <div id="App">
+      <div className="w-full lg:grid lg:min-h-[600px] xl:min-h-[800px]">
+        <div className="flex items-center justify-center py-12">
+          <div className="mx-auto grid gap-6">
+            <div className="grid gap-2 text-center">
+              <h1 className="text-3xl font-bold">Login</h1>
             </div>
+            <div className="grid gap-4">
+              <Button variant="outline" className="w-full">
+                Login with GitLab
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have a GitLab account?{" "}
+              <a href="#" className="underline">
+                Sign up
+              </a>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
